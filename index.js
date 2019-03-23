cf7cdd3e72015bbeda43841be2ff57e28f62b985
Arif Hossain <arifthecoder@gmail.com>
2019-03-23 18:58:07 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose')
+mongoose.set('useFindAndModify', false)
+const Person = require('./models/person')
+
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static('build'));
+
+
+const formatPerson = (person) => {
+    return {
+        name: person.name,
+        number: person.number,
+        id: person._id
+    }
+}
+
+
+
+app.get('/api/persons', (request, response) => {
+    Person
+        .find({})
+        .then(persons => {
+            response.json(persons.map(formatPerson))
+        })
+        .catch(error => {
+        response.status(400).send({error: `error in finding persons`})
+    })
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = (request.params.id)
+    Person.findById(id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else { response.status(404).end()}
+        })
+        .catch(error => {
+            response.status(400).send({error: `no ${id} found`})
+        })
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = (request.params.id)
+    Person
+        .findByIdAndRemove(id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => {
+            response.status(400).send({error: `no ${id} found`})
+        })
+})
+
+const generateId = () => {
+    let random = Math.random()+1
+    return parseInt(random*10000)
+}
+
+app.post('/api/persons', (request, response) => {
+    const person = new Person(request.body)
+    console.log(`reguest body: ${person}`)
+
+
+    if (person.name === '') {
+        console.log('fill name')
+        return response.status(400).json({error: 'name missing'})
+    }
+
+    if (person.number === '') {
+        console.log('fill number')
+        return response.status(400).json({error: 'number missing'})
+    }
+
+    person
+        .save()
+        .then(savedPerson => {
+            response.json(formatPerson(savedPerson))
+        })
+        .catch( error => {
+            response.status(400).send({error: `error in save`})
+        })
+    
+
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
