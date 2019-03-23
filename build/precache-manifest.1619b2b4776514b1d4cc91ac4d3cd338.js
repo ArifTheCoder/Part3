ba82e1f81375b38474829eb099dc366fabec2b1f
Arif Hossain <arifthecoder@gmail.com>
2019-03-23 19:52:53 +0200

    update

---
+++ build/precache-manifest.1619b2b4776514b1d4cc91ac4d3cd338.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "daf29d01c94e9a630036",
+    "url": "/static/js/main.6b99ec8b.chunk.js"
+  },
+  {
+    "revision": "bad115d63a11e08e9a3f",
+    "url": "/static/js/2.07535f92.chunk.js"
+  },
+  {
+    "revision": "daf29d01c94e9a630036",
+    "url": "/static/css/main.a8061819.chunk.css"
+  },
+  {
+    "revision": "84df72daf10d5ba0c482faeefe9c720e",
+    "url": "/index.html"
+  }
+];
---
+++ build/precache-manifest.246acd4a318ede4d830270bd508dbc84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "036543d0ce94f15f050d",
+    "url": "/static/js/main.aa358d4b.chunk.js"
+  },
+  {
+    "revision": "bad115d63a11e08e9a3f",
+    "url": "/static/js/2.07535f92.chunk.js"
+  },
+  {
+    "revision": "036543d0ce94f15f050d",
+    "url": "/static/css/main.bff7c3d9.chunk.css"
+  },
+  {
+    "revision": "227952695cbaa882b5043f389d360842",
+    "url": "/index.html"
+  }
+];
