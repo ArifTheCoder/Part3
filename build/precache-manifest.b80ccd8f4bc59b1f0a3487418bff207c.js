9e575cd3e12fbe0d3e3395d8e8b00b16fd92fee8
Arif Hossain <arifthecoder@gmail.com>
2019-03-23 20:01:40 +0200

    up

---
+++ build/precache-manifest.b80ccd8f4bc59b1f0a3487418bff207c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a723f46ac6b635e4bd1",
+    "url": "/static/js/main.5a723f46.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "52fbb79f2c8ddbb7f9bc",
+    "url": "/static/js/2.52fbb79f.chunk.js"
+  },
+  {
+    "revision": "8a3c9cda11b89cf36c92829741cb2c6e",
+    "url": "/index.html"
+  }
+];
